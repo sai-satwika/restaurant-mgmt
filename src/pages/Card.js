a6435e903ef08896f61749a6b5d47f044c905f5a
saisatwika <saisatwikabalivada@gmail.com>
2023-01-03 23:19:23 +0530

    update

--- src/pages/Card.js
+++ src/pages/Card.js
@@ -10,7 +10,7 @@ function BasicExample(props) {
         <Card.Title>Card Title</Card.Title>
         <Card.Text>
           Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          bulk of the card's contents.
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
